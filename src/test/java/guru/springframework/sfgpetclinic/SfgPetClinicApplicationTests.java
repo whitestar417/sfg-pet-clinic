f6398617e9bb445aca8e4312013a139584b638ba
David Haas <Dhaas3@mail.greenriver.edu>
2021-04-15 20:09:36 -0700

    Initial Project Add

---
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
